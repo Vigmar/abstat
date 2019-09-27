3bfba5a14f40416977618265319b38ae32aa5722
Vigmar <wins.dynpic@gmail.com>
2019-09-27 18:59:51 +0300

    added react-vis + 3 charts

--- src/SellPointPage/SellPointPage.js
+++ src/SellPointPage/SellPointPage.js
@@ -11,10 +11,57 @@ import Button from '@material-ui/core/Button';
 import { withStyles } from "@material-ui/core/styles";
 
 import { AppBar } from '../AppBar/AppBar';
+import '../../node_modules/react-vis/dist/style.css';
 
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, VerticalBarSeries, VerticalGridLines, MarkSeries, ArcSeries,Sunburst } from 'react-vis';
 
 import Switch from "@material-ui/core/Switch";
 
+const myData = {
+ "title": "analytics",
+ "color": "#12939A",
+ "children": [
+  {
+   "title": "cluster",
+   "children": [
+    {"title": "AgglomerativeCluster", "color": "#12939A", "size": 3938},
+    {"title": "CommunityStructure", "color": "#12939A", "size": 3812},
+    {"title": "HierarchicalCluster", "color": "#12939A", "size": 6714},
+    {"title": "MergeEdge", "color": "#12939A", "size": 743}
+   ]
+  },
+  {
+   "title": "graph",
+   "children": [
+    {"title": "BetweennessCentrality", "color": "#12939A", "size": 3534},
+    {"title": "LinkDistance", "color": "#12939A", "size": 5731},
+    {"title": "MaxFlowMinCut", "color": "#12939A", "size": 7840},
+    {"title": "ShortestPaths", "color": "#12939A", "size": 5914},
+    {"title": "SpanningTree", "color": "#12939A", "size": 3416}
+   ]
+  },
+  {
+   "title": "optimization",
+   "children": [
+    {"title": "AspectRatioBanker", "color": "#12939A", "size": 7074}
+   ]
+  }
+ ]
+}
+
+
+  const data = [
+      {x: 0, y: 8},
+      {x: 1, y: 5},
+      {x: 2, y: 4},
+      {x: 3, y: 9},
+      {x: 4, y: 1},
+      {x: 5, y: 7},
+      {x: 6, y: 6},
+      {x: 7, y: 3},
+      {x: 8, y: 2},
+      {x: 9, y: 0}
+    ];
 
 
 class SellPointPage extends React.Component {
@@ -49,7 +96,18 @@ class SellPointPage extends React.Component {
 	
        
         return (
-		<div>TEST</div>
+		<div>
+		<div><XYPlot height={200} width={200}>
+  <VerticalBarSeries data={data} />
+</XYPlot>
+<XYPlot height={200} width={200}>
+  <LineSeries data={data} />
+</XYPlot>
+<XYPlot height={200} width={200}>
+  <MarkSeries data={data} />
+</XYPlot>
+
+</div></div>
         );
     }
 }
